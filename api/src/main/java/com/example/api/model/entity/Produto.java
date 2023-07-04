2391e80734a89aabab3946309289276e811d3f8f
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2023-07-04 20:33:57 -0300

    Commit inicial.

---
+++ api/src/main/java/com/example/api/model/entity/Produto.java
@@ -0,0 +1,81 @@
+package com.example.api.model.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto", schema = "mercado")
+public class Produto {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@Column(name = "quantidade" , nullable = false)
+	private Integer quantidade;
+	
+	@Column(name = "valor", nullable = false)
+	private Long valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Long getValor() {
+		return valor;
+	}
+
+	public void setValor(Long valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, quantidade, valor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Objects.equals(quantidade, other.quantidade) && Objects.equals(valor, other.valor);
+	}
+	
+	
+}
